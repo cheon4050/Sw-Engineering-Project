1f132a1442f245a75d50e82a4977713a49256859
SunWoong <sunwoong1122@gmail.com>
2022-05-21 16:22:39 +0900

    feat: 의류 조회 RecyclerView 구현

---
+++ android/app/src/main/java/com/maker/hanger/adapter/SearchRVAdapter.kt
@@ -0,0 +1,33 @@
+package com.maker.hanger.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.maker.hanger.data.Clothes
+import com.maker.hanger.databinding.ItemClothesBinding
+
+class SearchRVAdapter(private val clothes: ArrayList<Clothes>) : RecyclerView.Adapter<SearchRVAdapter.ViewHolder>() {
+    interface OnItemClickListener {
+        fun onDetailedInfoItem()
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemClothesBinding = ItemClothesBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(clothes[position])
+    }
+
+    override fun getItemCount(): Int = clothes.size
+
+    inner class ViewHolder(private val binding: ItemClothesBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(clothes: Clothes) {
+            binding.clothesKindTv.text = clothes.kind.toString()
+            binding.clothesSizeTv.text = clothes.size.toString()
+        }
+    }
+}
