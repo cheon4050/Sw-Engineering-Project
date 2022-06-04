c76b8bd66e5919c22fae7f545a63ffcc86cff525
Ujin28 <melon4092@naver.com>
2022-06-04 19:05:54 +0900

    AuthRepository update

--- server/src/main/java/maker/server/Auth/AuthRepository.java
+++ server/src/main/java/maker/server/Auth/AuthRepository.java
@@ -6,7 +6,13 @@ import maker.server.Dto.User.UserLoginDto;
 import maker.server.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.SqlValue;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.SQLData;
+import java.sql.SQLInput;
+import java.sql.SQLType;
 
 @Repository
 public class AuthRepository {
@@ -15,6 +21,7 @@ public class AuthRepository {
     private JdbcTemplate jdbcTemplate;
 
     public void save(UserDto user) {
+
         jdbcTemplate.execute("INSERT INTO user(userId,password,birth) VALUES ('"+
                 user.getUserId()+ "','"+
                 user.getPassword()+ "','"+
@@ -22,16 +29,23 @@ public class AuthRepository {
     }
 
     public String findUserToken(UserLoginDto user){
+        jdbcTemplate.execute("select userToken from user where userId = '" +
+                user.getUserId()+"' and password = '"+ user.getPassword() + "'");
         return "";
     }
 
     public String findPassword(UserFindDto user){
+        jdbcTemplate.execute("select password from user where userId = '" +
+                user.getUserId() + "' and birth = '" + user.getBirth() + "'");
         return "";
     }
     public void update(UserDto user){
+        jdbcTemplate.execute("update user set userId = '" + user.getUserId() + "' and password = '" + user.getPassword() + "' and birth = '" + user.getBirth() + "'");
 
     }
     public void delete(String userToken) {
+        jdbcTemplate.execute("delete from user where userToken = '" +
+                userToken + "'");
 
     }
 }
