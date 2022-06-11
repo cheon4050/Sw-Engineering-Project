f46de4776aacf530ac9933d22a24877314124d93
cheon4050 <ehdcjs159@gmail.com>
2022-06-11 22:53:01 +0900

    chore: null값 처리 코드 수정

--- server/src/main/java/maker/server/Entity/Clothes.java
+++ server/src/main/java/maker/server/Entity/Clothes.java
@@ -24,22 +24,22 @@ public class Clothes {
     private boolean bookmark;
 
     public void setSeason(String strSeason){
-        if(strSeason.equals("null"))
-            this.season = null;
+        if(strSeason.equals("[]"))
+            this.season = new ArrayList<String>();
         else
             this.season = new ArrayList<String>(Arrays.asList(strSeason.substring(1,strSeason.length()-1).split(", ")));
     }
 
     public void setKind(String strKind){
-        if(strKind.equals("null"))
-            this.kind = null;
+        if(strKind.equals("[]"))
+            this.kind = new ArrayList<String>();
         else
             this.kind = new ArrayList<String>(Arrays.asList(strKind.substring(1,strKind.length()-1).split(", ")));
     }
 
     public void setWashingMethod(String str){
-        if (str.equals("null"))
-                this.washingMethod = null;
+        if (str.equals("[]"))
+                this.washingMethod = new ArrayList<Integer>();
         else {
             int[] arr = Arrays.stream(str.substring(1, str.length() - 1).split(", ")).map(String::trim).mapToInt(Integer::parseInt).toArray();
             this.washingMethod = (ArrayList<Integer>) Arrays.stream(arr).boxed().collect(Collectors.toList());
