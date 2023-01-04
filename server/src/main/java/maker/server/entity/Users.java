adec4f2c49b20e92843e516e6ce9ae97abacfde9
cheon4050 <ehdcjs159@gmail.com>
2023-01-05 05:38:08 +0900

    refactor: Users Entity jpa Entity로 변경

--- server/src/main/java/maker/server/entity/Users.java
+++ server/src/main/java/maker/server/entity/Users.java
@@ -2,29 +2,42 @@ package maker.server.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userIdx;
+    private Integer Id;
 
-    private String userId;
+    private String email;
 
     private String password;
 
     private String answer;
 
+    @OneToMany
+    @JoinColumn(name="userIdx")
+    private List<Clothes> clothes;
+
     @Builder
-    public User(String userId, String password, String answer) {
-        this.userId = userId;
+    public Users(String email, String password, String answer) {
+        this.email = email;
         this.password = password;
         this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "Id=" + Id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", answer='" + answer + '\'' +
+                ", clothes=" + clothes +
+                '}';
     }
 }
