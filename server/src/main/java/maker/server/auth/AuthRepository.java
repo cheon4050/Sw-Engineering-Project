c4d8abaef2d48197eb73d3a320d72a630230e664
cheon4050 <ehdcjs159@gmail.com>
2023-01-05 05:40:05 +0900

    style: user.getuserId() -> user.getEmail() 변경

--- server/src/main/java/maker/server/auth/AuthRepository.java
+++ server/src/main/java/maker/server/auth/AuthRepository.java
@@ -1,6 +1,6 @@
 package maker.server.auth;
 
-import maker.server.dto.user.UserDto;
+import maker.server.dto.user.UserSaveDto;
 import maker.server.dto.user.UserFindDto;
 import maker.server.dto.user.UserLoginDto;
 import maker.server.dto.user.UserUpdateDto;
@@ -23,28 +23,28 @@ public class AuthRepository {
         return rs.getInt(1);
     };
 
-    public void save(UserDto user) {
+    public void save(UserSaveDto user) {
 
         jdbcTemplate.execute("INSERT INTO user(userId,password,answer) VALUES ('"+
-                user.getUserId()+ "','"+
+                user.getEmail()+ "','"+
                 user.getPassword()+ "','"+
                 user.getAnswer()+ "')");
     }
 
     public int findUserIdx(UserLoginDto user) {
         return jdbcTemplate.queryForObject("select userIdx from user where userId = '"
-                +user.getUserId()+"'"
+                +user.getEmail()+"'"
                 , intMapper);
 
     }
 
     public String findPassword(UserFindDto user){
         return jdbcTemplate.queryForObject("select password from user where userId = '" +
-                        user.getUserId() + "' and answer = '" + user.getAnswer() + "'",
+                        user.getEmail() + "' and answer = '" + user.getAnswer() + "'",
                 stringMapper);
     }
     public void update(Integer userIdx,UserUpdateDto user){
-        jdbcTemplate.execute("update user set userId = '" + user.getUserId() +
+        jdbcTemplate.execute("update user set userId = '" + user.getEmail() +
                 "', password = '" + user.getPassword() +
                 "' where userIdx = " + userIdx);
     }
