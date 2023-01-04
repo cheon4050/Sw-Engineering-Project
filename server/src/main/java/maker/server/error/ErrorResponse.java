5fc1c4a5ccda0697203636b6a94d7a2254f0608c
cheon4050 <ehdcjs159@gmail.com>
2023-01-05 05:36:11 +0900

    refactor: 에러 반환 타입 수정

---
+++ server/src/main/java/maker/server/error/ErrorResponse.java
@@ -0,0 +1,21 @@
+package maker.server.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode){
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                    .status(errorCode.getHttpStatus().value())
+                    .message(errorCode.getMessage())
+                    .build());
+    }
+}
